b583735e6e6884414aa9dc0bbef057f6236eca93
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-10-15 13:30:21 -0500

    add variables.tf

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "vms" {
+  description = "Map of VMs to create."
+  type = map(object({
+    virtual_environment_endpoint  = string
+    virtual_environment_api_token = string
+    virtual_environment_username  = string
+    node_name                     = string
+    datastore_id                  = string
+    vm_name                       = string
+    vm_hostname                   = string
+    description                   = string
+    tags                          = list(string)
+    vm_on_boot                    = bool
+    iso_path                      = string
+    operating_system              = string
+    cpu                           = number
+    memory                        = number
+    default_user                  = string
+    users = map(object({
+      password            = string
+      ssh_authorized_keys = list(string)
+    }))
+    tpm_version = string
+  }))
+}
