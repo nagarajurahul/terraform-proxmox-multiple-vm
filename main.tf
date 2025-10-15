c686506ec6d06aa7ca9559c649d3412d0ca3d37b
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-10-15 13:29:15 -0500

    add module source and code

--- main.tf
+++ main.tf
@@ -20,3 +20,29 @@ provider "proxmox" {
     username = var.vms[local.first].virtual_environment_username
   }
 }
+
+module "vm" {
+  source = "git::https://github.com/nagarajurahul/terraform-proxmox-vm-module.git?ref=v1.0.0"
+
+  for_each = var.vms
+
+  virtual_environment_endpoint  = each.value.virtual_environment_endpoint
+  virtual_environment_api_token = each.value.virtual_environment_api_token
+  virtual_environment_username  = each.value.virtual_environment_username
+
+  node_name    = each.value.node_name
+  datastore_id = each.value.datastore_id
+  vm_name      = each.value.vm_name
+  vm_hostname  = each.value.vm_hostname
+  description  = each.value.description
+  tags         = each.value.tags
+  vm_on_boot   = each.value.vm_on_boot
+
+  iso_path         = each.value.iso_path
+  operating_system = each.value.operating_system
+  cpu              = each.value.cpu
+  memory           = each.value.memory
+  default_user     = each.value.default_user
+  users            = each.value.users
+  tpm_version      = each.value.tpm_version
+}
