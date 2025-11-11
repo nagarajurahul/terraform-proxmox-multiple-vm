bf821c16098808518bc77fbf627128d4426808c3
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-11-11 13:22:36 -0600

    fix: module version to 1.4.5 to dynamically choose interface, so IP will be allocated

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ provider "proxmox" {
 }
 
 module "vm" {
-  source = "git::https://github.com/nagarajurahul/terraform-proxmox-vm-module.git?ref=v1.4.4"
+  source = "git::https://github.com/nagarajurahul/terraform-proxmox-vm-module.git?ref=v1.4.5"
 
   for_each = var.vms
 
