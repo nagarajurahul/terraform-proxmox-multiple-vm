9052c8c7cfe80208efb8c5be26c3da5ae028c001
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-11-21 11:30:58 -0600

    add locals file to merge vm and group variables in core-services

---
+++ environments/prod/core-services/locals.tf
@@ -0,0 +1,19 @@
+locals {
+  # Merge and flatten core-services groups
+  all_vms = merge([
+    for group, vms in var.vm_groups : {
+      for vm_key, vm in vms :
+      "${group}-${vm_key}" => merge(
+        vm,
+        {
+          group        = group
+          node_name    = coalesce(vm.node_name, var.vm_defaults.node_name)
+          datastore_id = coalesce(vm.datastore_id, var.vm_defaults.datastore_id)
+          iso_path    = coalesce(vm.iso_path, var.vm_defaults.iso_path)
+          operating_system    = coalesce(vm.operating_system, var.vm_defaults.operating_system)
+          environment    = coalesce(vm.environment, var.vm_defaults.environment)
+        }
+      )
+    }
+  ]...)
+}
